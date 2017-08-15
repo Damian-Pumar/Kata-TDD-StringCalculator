8cfd150aad349e85ddc706da1763da95e200c1a4
Damián Pumar <damianpumar@gmail.com>
2017-08-15 11:04:01 -0300

    First commit

---
+++ TDD.Kata.StringCalculator/TDD.Kata.StringCalculator.Tests.Unit/StringCalculatorTests.cs
@@ -0,0 +1,221 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TDD.Kata.StringCalculator.Tests.Unit
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        private StringCalculator Target;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.Target = new StringCalculator();
+        }
+
+        [TestMethod]
+        public void add_emptystring_zero()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("");
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void add_singlenumber_returnsThatNumber_1()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("1");
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void add_single_number_returns_that_number_2()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("2");
+
+            //Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void add_two_numbers_sums_them_1()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("1,2");
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void add_two_numbers_sums_them_2()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("1,3");
+
+            //Assert
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void add_two_larger_numbers_sums_them()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("10,30");
+
+            //Assert
+            Assert.AreEqual(40, result);
+        }
+
+        [TestMethod]
+        public void add_new_line_zero()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("\n");
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void add_new_line_be_tween_numbers_treats_as_seperator()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("1\n2");
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void add_different_delimiter_used_to_parse_the_sum()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("//A\n1A2");
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void add_different_delimiter_used_to_parse_the_sum_2()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("//B\n1B2");
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void add_negative_throws()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("-1");
+
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void add_multiple_negative_throws()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("-1,-2");
+
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void add_negativeandpositive_throwsonlythenegative()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("-1,2");
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void add_numbers_bigger_than_1000_ignored()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("2,1001");
+
+            //Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void add_delimiters_longer_than_one_char_still_accepted()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("//AAA\n1AAA2AAA3");
+
+            //Assert
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void add_multiple_delimiters_all_used()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("//[A][b]\n1A2b3");
+
+            //Assert
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void add_multiple_delimiters_all_used_2()
+        {
+            //Arrange
+
+            //Act
+            var result = Target.Add("//[AB][bc]\n1AB2bc3");
+
+            //Assert
+            Assert.AreEqual(6, result);
+        }
+    }
+}
---
+++ TDD.Kata.StringCalculator/TDD.Kata.StringCalculator/StringCalculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TDD.Kata.StringCalculator
+{
+    public class StringCalculator
+    {
+        private const Int32 defaultNumber = 0;
+
+        private const String customSeparator = "//";
+
+        private List<String> separators = new List<String>() { ",", "\n" };
+
+        private const Int32 maxNumber = 1000;
+
+        public Int32 Add(String numbers)
+        {
+            if (String.IsNullOrWhiteSpace(numbers))
+                return defaultNumber;
+
+            if (numbers.StartsWith(customSeparator))
+                numbers = this.AddCustomSeperators(numbers);
+
+            var cleanedNumbers = this.CleanNumbers(numbers);
+
+            return cleanedNumbers.Sum();
+        }
+
+        private String AddCustomSeperators(String numbers)
+        {
+            String[] customSeperators = { customSeparator, "[", "]" };
+
+            var customSeperator = numbers.Split(new String[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).First();
+
+            numbers = numbers.Substring(customSeperator.Length, numbers.Length - customSeperator.Length);
+
+            var allCustomSeperators = customSeperator.Split(customSeperators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var sep in allCustomSeperators)
+                separators.Add(sep);
+
+            return numbers;
+        }
+
+        private IList<Int32> CleanNumbers(String numbers)
+        {
+            var nums = numbers.Split(separators.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            var cleaned = new List<Int32>();
+
+            foreach (var num in nums)
+            {
+                var cleanedNumber = Int32.Parse(num);
+
+                if (cleanedNumber < 0)
+                {
+                    throw new ApplicationException("Number cannot be negative");
+                }
+
+                if (cleanedNumber <= maxNumber)
+                {
+                    cleaned.Add(cleanedNumber);
+                }
+            }
+
+            return cleaned;
+        }
+    }
+}
