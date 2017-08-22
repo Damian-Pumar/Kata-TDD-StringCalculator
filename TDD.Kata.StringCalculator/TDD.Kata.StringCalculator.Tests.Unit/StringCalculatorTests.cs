0811940c67c290ec1f1ed5813d96bd0f82edf322
Damián Pumar <damianpumar@gmail.com>
2017-08-22 13:41:43 -0300

    implemented xnuit for ci build

--- TDD.Kata.StringCalculator/TDD.Kata.StringCalculator.Tests.Unit/StringCalculatorTests.cs
+++ TDD.Kata.StringCalculator/TDD.Kata.StringCalculator.Tests.Unit/StringCalculatorTests.cs
@@ -1,221 +1,216 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
+using Xunit;
 
 namespace TDD.Kata.StringCalculator.Tests.Unit
 {
-    [TestClass]
     public class StringCalculatorTests
     {
         private StringCalculator Target;
 
-        [TestInitialize]
-        public void Initialize()
+        public StringCalculatorTests()
         {
             this.Target = new StringCalculator();
         }
 
-        [TestMethod]
+        [Fact]
         public void add_emptystring_zero()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("");
+            var result = this.Target.Add("");
 
             //Assert
-            Assert.AreEqual(0, result);
+            Assert.Equal(0, result);
         }
 
-        [TestMethod]
+        [Fact]
         public void add_singlenumber_returnsThatNumber_1()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("1");
+            var result = this.Target.Add("1");
 
             //Assert
-            Assert.AreEqual(1, result);
+            Assert.Equal(1, result);
         }
 
-        [TestMethod]
+        [Fact]
         public void add_single_number_returns_that_number_2()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("2");
+            var result = this.Target.Add("2");
 
             //Assert
-            Assert.AreEqual(2, result);
+            Assert.Equal(2, result);
         }
 
-        [TestMethod]
+        [Fact]
         public void add_two_numbers_sums_them_1()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("1,2");
+            var result = this.Target.Add("1,2");
 
             //Assert
-            Assert.AreEqual(3, result);
+            Assert.Equal(3, result);
         }
 
-        [TestMethod]
+        [Fact]
         public void add_two_numbers_sums_them_2()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("1,3");
+            var result = this.Target.Add("1,3");
 
             //Assert
-            Assert.AreEqual(4, result);
+            Assert.Equal(4, result);
         }
 
-        [TestMethod]
+        [Fact]
         public void add_two_larger_numbers_sums_them()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("10,30");
+            var result = this.Target.Add("10,30");
 
             //Assert
-            Assert.AreEqual(40, result);
+            Assert.Equal(40, result);
         }
 
-        [TestMethod]
+        [Fact]
         public void add_new_line_zero()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("\n");
+            var result = this.Target.Add("\n");
 
             //Assert
-            Assert.AreEqual(0, result);
+            Assert.Equal(0, result);
         }
 
-        [TestMethod]
+        [Fact]
         public void add_new_line_be_tween_numbers_treats_as_seperator()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("1\n2");
+            var result = this.Target.Add("1\n2");
 
             //Assert
-            Assert.AreEqual(3, result);
+            Assert.Equal(3, result);
         }
 
-        [TestMethod]
+        [Fact]
         public void add_different_delimiter_used_to_parse_the_sum()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("//A\n1A2");
+            var result = this.Target.Add("//A\n1A2");
 
             //Assert
-            Assert.AreEqual(3, result);
+            Assert.Equal(3, result);
         }
 
-        [TestMethod]
+        [Fact]
         public void add_different_delimiter_used_to_parse_the_sum_2()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("//B\n1B2");
+            var result = this.Target.Add("//B\n1B2");
 
             //Assert
-            Assert.AreEqual(3, result);
+            Assert.Equal(3, result);
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ApplicationException))]
+        [Fact]
         public void add_negative_throws()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("-1");
+            Assert.Throws<ApplicationException>(() => this.Target.Add("-1"));
 
             //Assert
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ApplicationException))]
+        [Fact]
         public void add_multiple_negative_throws()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("-1,-2");
+            Assert.Throws<ApplicationException>(() => this.Target.Add("-1,-2"));
 
             //Assert
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ApplicationException))]
+        [Fact]
         public void add_negativeandpositive_throwsonlythenegative()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("-1,2");
+            Assert.Throws<ApplicationException>(() => this.Target.Add("-1,2"));
 
             //Assert
         }
 
-        [TestMethod]
+        [Fact]
         public void add_numbers_bigger_than_1000_ignored()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("2,1001");
+            var result = this.Target.Add("2,1001");
 
             //Assert
-            Assert.AreEqual(2, result);
+            Assert.Equal(2, result);
         }
 
-        [TestMethod]
+        [Fact]
         public void add_delimiters_longer_than_one_char_still_accepted()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("//AAA\n1AAA2AAA3");
+            var result = this.Target.Add("//AAA\n1AAA2AAA3");
 
             //Assert
-            Assert.AreEqual(6, result);
+            Assert.Equal(6, result);
         }
 
-        [TestMethod]
+        [Fact]
         public void add_multiple_delimiters_all_used()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("//[A][b]\n1A2b3");
+            var result = this.Target.Add("//[A][b]\n1A2b3");
 
             //Assert
-            Assert.AreEqual(6, result);
+            Assert.Equal(6, result);
         }
 
-        [TestMethod]
+        [Fact]
         public void add_multiple_delimiters_all_used_2()
         {
             //Arrange
 
             //Act
-            var result = Target.Add("//[AB][bc]\n1AB2bc3");
+            var result = this.Target.Add("//[AB][bc]\n1AB2bc3");
 
             //Assert
-            Assert.AreEqual(6, result);
+            Assert.Equal(6, result);
         }
     }
 }
